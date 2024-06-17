69ee7aa88c30a872e4290cc9611d45b23dd9975f
Giulio-Marra <giuliomarramail@gmail.com>
2024-06-17 14:13:35 +0200

    creazione progetto con spring

---
+++ giulio_marra/src/main/java/giulio/marra/giulio_marra/GiulioMarraApplication.java
@@ -0,0 +1,13 @@
+package giulio.marra.giulio_marra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiulioMarraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiulioMarraApplication.class, args);
+	}
+
+}
